956770ac9aa879bac2277dab28100ca20ba0936d
Josh-Manley <joshmanley45@gmail.com>
2024-02-21 23:48:53 -0700

    added some updats to init function

--- Readme-generator/Develop/index.js
+++ Readme-generator/Develop/index.js
@@ -1,26 +1,29 @@
 // TODO: Include packages needed for this application
-const markdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
-  inquirer
-    .prompt({
+    {
       type: 'Title',
       message: 'What is title of readme?',
       name: 'title'
-    },
-
-      
-    )
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  FileSystem.writeFile('README.md',)
+  fs.writeFile(fileName, data);
+  err ? console.log('error') : console.log('complete');
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+  const markdownContent = generateMarkdown(answers);
+  writeToFile('README.md', markdownContent)
+});
+}
 
 // Function call to initialize app
 init();
