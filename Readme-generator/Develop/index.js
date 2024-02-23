962d681b189b0459832c4ced95f3a66a845ebb2d
Josh-Manley <joshmanley45@gmail.com>
2024-02-22 23:11:25 -0700

    added more sections and adding licenses

--- Readme-generator/Develop/index.js
+++ Readme-generator/Develop/index.js
@@ -5,22 +5,49 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'Title',
+      type: 'title',
       message: 'What is title of readme?',
       name: 'title'
+    },
+    {
+      type: 'input',
+      message: 'Description',
+      name: 'description'
+    },
+    // table of contents
+    {
+      type: 'input',
+      message: 'Installation',
+      name: 'installation'
+    },
+    {
+      type: 'input',
+      message: 'Usage',
+      name: 'usage'
+    },
+    {
+      type: 'list',
+      message: 'Choose License',
+      choices: ['MIT', 'Apache License 2.0', 'The Unlicense'],
+      name: 'license'
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data);
-  err ? console.log('error') : console.log('complete');
-}
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      console.log('Complete');
+    }
+  }
+)}
 
 // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((answers) => {
-  const markdownContent = generateMarkdown(answers);
+  const markdownContent = generateMarkdown.generateMarkdown(answers);
   writeToFile('README.md', markdownContent)
 });
 }
